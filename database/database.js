7a705d7ffd22a647e18d48dd512b2751db646130
caionikolas <Caio.kanu@hotmail.com>
2023-04-16 21:40:34 -0300

    projeto

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize")
-const connection = new Sequelize('controleinvestimentos','root','bakacris0',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
-
