80c8e4f9c86964e7ad66211cc008989849065cd7
ChrisFelliss <112284232+ChrisFelliss@users.noreply.github.com>
2023-04-23 18:56:23 -0300

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,39 +1,53 @@
-var valor_acao = 49.02
-setInterval(attValorAcao, 10000);
-
+const lista = [];
 function adicionarLinha() {
+    var codigo_unico = document.getElementById("cod").value;
+    const regex = /^[A-Za-z]{4}[0-9]{1,2}$/;
+
+
+    if (!regex.test(codigo_unico)) {
+        alert('A string não segue a formatação adequada.');
+        return;
+    } else {
+        for (let i = 0; i < lista.length; i++) {
+            if (lista[i] == codigo_unico) {
+              alert("Código já existente!");
+              return; // encerra a função
+            }
+          }
+    }
+
+
     var qnt = document.getElementById("qnt").value;
     if (qnt.trim() == "") {
-        alert("Por favor, preencha o campo.");
-        return false;
-    } else if (isNaN(qnt)) {
-        alert("Por favor, digite apenas números.");
+        alert("Por favor, preencha o campo 'Quantidade' vazio.");
         return false;
     }
-    var numero_aleatorio = Math.floor(Math.random() * 100000); // Gera um número aleatório de 5 dígitos
-    var codigo_unico = "COD-" + numero_aleatorio.toString(); // Cria o código único com o número aleatório
+    var valor_acao = document.getElementById("ativo").value;
     var funcao = document.querySelector('input[name="funcao"]:checked').value;
-    var tx_corretagem = 0.00027;
-    var valor_op = qnt * valor_acao ;
-    var imposto = valor_op * tx_corretagem;
-    if (funcao == 'Compra') {
+    
+    var tx_corretagem = document.getElementById("tx").value;
+    alert(tx_corretagem)
+    var valor_op = qnt * valor_acao;
+    var imposto = 2;
+
+    if (funcao == "Compra") {
         var valor_final = (valor_op + imposto).toFixed(2);
-        op = '-'
+        op = '▲';
     } else if (funcao == "Venda") {
         var valor_final = (valor_op - imposto).toFixed(2);
-        op = '+'
+        op = '▼';
     }
-    
-    const dataAtual = new Date();
-    const dia = dataAtual.getDate().toString().padStart(2, '0');
-    const mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0'); // Observe que os meses começam em 0
-    const ano = dataAtual.getFullYear();
-    const dataFormatada = `${dia}/${mes}/${ano}`;
 
-    // Seleciona a tabela e adiciona uma nova linha
+    // Seleciona a tabela e adiciona uma nova linha abaixo da última linha
+    var data = document.getElementById("data").value
+    if (data.trim() == "") {
+        alert("Por favor, preencha a data.");
+        return false;
+    }
+    data = moment(data, "YYYY-MM-DD").format("DD/MM/YYYY");
     var tabela = document.querySelector("table");
     var linha = tabela.insertRow();
-
+    lista.push(codigo_unico);
     // Adiciona as células na nova linha
     var celulaCod = linha.insertCell();
     var celuladate = linha.insertCell();
@@ -47,32 +61,14 @@ function adicionarLinha() {
 
     // Define o valor das células
     celulaCod.innerHTML = codigo_unico;
-    celuladate.innerHTML = dataFormatada;
+    celuladate.innerHTML = data;
     celulaqnt.innerHTML = qnt;
     celulafuncao.innerHTML = funcao;
     celulavalor_acao.innerHTML = `$ ${valor_acao}`;
-    celulatx_corretagem.innerHTML = tx_corretagem*100;
+    celulatx_corretagem.innerHTML = tx_corretagem;
     celulavalor_op.innerHTML = `R$ ${valor_op.toFixed(2)} `;
     celulaimposto.innerHTML = imposto.toFixed(2);
-    celulavalor_final.innerHTML = `R$  ${op + valor_final}`;
-}
+    celulavalor_final.innerHTML = `R$  ${valor_final}`;
 
-function attValorAcao() {
-    // código para atualizar o valor da ação...
-
-
-        
-    const porcentagem = Math.floor(parseFloat((Math.random() * 15).toFixed(2))); // porcentagem aleatória entre 0 e 100
-    const operacao = Math.random() < 0.5 ? '+' : '-'; // operação aleatória '+' ou '-'
     
-    if (operacao === '+') {
-        valor_acao += valor_acao * (porcentagem / 100); // adiciona a porcentagem ao valor
-    } else {
-        valor_acao -= valor_acao * (porcentagem / 100); // subtrai a porcentagem do valor
-    }
-    valor_acao = parseFloat((parseFloat(valor_acao)).toFixed(2))
-    const spanValor = document.getElementById("valor_acao");
-    spanValor.textContent = "$" + valor_acao.toFixed(2);
 }
-document.getElementById("valor_acao").innerHTML = '$'+valor_acao;
-setInterval(minhaFuncao, 1000);
